8f78e527d6f1fde24aa86c10831f6d01d9832789
Nrycord <39805471+Nrycord@users.noreply.github.com>
2019-02-22 17:43:25 -0600

    Add files via upload

---
+++ Tareas/Guia_Programacion-II/Ej3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <conio.h> 
+#include <iostream>
+using namespace std;
+//eleva el numero deseado a un exponente que el usuario ingrese
+float potencia(double n, int p);
+
+int main(){ 
+	double n;
+	int p;
+	cout << "Ingrese el numero a elevar: "; cin >> n;//ingresa el numero a elevar
+	cout << "Ingrese la pontencia a emplear: "; cin >> p;//ingresa la potencia del numero
+	cout << "El resultado es: " << potencia(n,p) << endl;
+}
+
+float potencia(double n, int p)
+{
+	int i = 1;
+	double respuesta = 1;
+	while(i <= p){//El ciclo se repite "p" veces
+		respuesta = respuesta * n; //multiplica el numero repetidamente
+		i++;
+	}
+	return respuesta;//retorna el valor resultante
+}
---
+++ Tareas/Guia_Programacion-II/Ej5.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h> 
+#include <conio.h> 
+#include <iostream>
+using namespace std;
+//evalua cual de los numeros es el menos de dos opciones, asigna el valor de 0 al menor
+int menorACero(int&,int&);
+void cl(int &);
+
+int main(){ 
+	int a,b;
+	cout << "Ingrese el primer numero: "; cin >> a;
+	cl(a);
+	cout << "Ingrese el segundo numero: "; cin >> b;
+	cl(b);
+	menorACero(a,b);
+	cout << "El valor a = " << a << "\nEl valor b = " << b << endl;
+}
+
+int menorACero(int &a,int &b){
+	if(b > a){
+		a = 0;
+		cout << "El primer numero es el menor" << endl;
+	}
+	else if(a > b){
+		b = 0;
+		cout << "El segundo numero es el menor" << endl;
+	} 
+	else{
+		cout << "Ambos numeros son iguales" << endl;
+	}
+}
+
+void cl(int &a){
+	if(cin.fail()){
+		cin.clear();
+		cin.ignore();
+		a = 0;
+		cout << "ingrese un numero entero." << endl;
+		system("pause");
+		exit(1);
+	}
+}
